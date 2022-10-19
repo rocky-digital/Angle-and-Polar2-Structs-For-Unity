34add632d40dfacb87e96bbf7d1395bbf52643a3
rocky-digital <jamesroquelara12@gmail.com>
2022-10-19 18:30:44 -0500

    PropertySigned and PropertyUnsigned passed

--- Assets/Scripts/Tests/AngleTests.cs
+++ Assets/Scripts/Tests/AngleTests.cs
@@ -6,11 +6,40 @@ using UnityEngine.TestTools;
 
 public class AngleTests
 {
+    // Component Properties:
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     [Test]
     public void PropertyDegrees()
     {
-        Angle angle1 = new() { Degrees = 90f };
-        Assert.AreEqual(90f, angle1.Degrees);
+        Angle angle1 = new() { Degrees = 1f };
+        Assert.AreEqual(1f, angle1.Degrees);
+        angle1.Degrees = -1f;
+        Assert.AreEqual(-1f, angle1.Degrees);
+    }
+
+    // Read Only Properties:
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    [Test]
+    public void PropertyUnsigned()
+    {
+        Angle angle1 = new() { Degrees = -90f };
+        Assert.AreEqual(270f, angle1.Unsigned.Degrees);
+
+        angle1.Degrees = 1080f;
+        Assert.AreEqual(0, angle1.Unsigned.Degrees);
+    }
+
+    [Test]
+    public void PropertySigned()
+    {
+        Angle angle1 = new() { Degrees = 315f };
+        Assert.AreEqual(-45, angle1.Signed.Degrees);
+
+        angle1.Degrees = 720f;
+        Assert.AreEqual(0, angle1.Unsigned.Degrees);
     }
 
     [Test]
