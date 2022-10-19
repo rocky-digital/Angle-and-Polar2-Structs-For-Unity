b21834a5a2ba2e967c666cbe460aa434eb80cb49
rocky-digital <jamesroquelara12@gmail.com>
2022-10-19 18:39:38 -0500

    comments added

--- Assets/Scripts/Tests/AngleTests.cs
+++ Assets/Scripts/Tests/AngleTests.cs
@@ -21,13 +21,18 @@ public class AngleTests
     // Read Only Properties:
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
     [Test]
     public void PropertyUnsigned()
     {
-        Angle angle1 = new() { Degrees = -90f };
+        // Test setting positive angles
+        Angle angle1 = new() { Degrees = 30f };
+        Assert.AreEqual(30f, angle1.Unsigned.Degrees);
+
+        // Test setting negative angles
+        angle1.Degrees = -90f;
         Assert.AreEqual(270f, angle1.Unsigned.Degrees);
 
+        // Test setting angles greater than 360 degrees
         angle1.Degrees = 1080f;
         Assert.AreEqual(0, angle1.Unsigned.Degrees);
     }
@@ -35,11 +40,17 @@ public class AngleTests
     [Test]
     public void PropertySigned()
     {
-        Angle angle1 = new() { Degrees = 315f };
-        Assert.AreEqual(-45, angle1.Signed.Degrees);
+        // Test setting positive angles
+        Angle angle1 = new() { Degrees = 10f };
+        Assert.AreEqual(10f, angle1.Signed.Degrees);
 
-        angle1.Degrees = 720f;
-        Assert.AreEqual(0, angle1.Unsigned.Degrees);
+        // Test setting negative angles
+        angle1.Degrees = 315f;
+        Assert.AreEqual(-45f, angle1.Signed.Degrees);
+
+        // Test setting angles greater than 360 degrees
+        angle1.Degrees = 765f;
+        Assert.AreEqual(45f, angle1.Unsigned.Degrees);
     }
 
     [Test]
