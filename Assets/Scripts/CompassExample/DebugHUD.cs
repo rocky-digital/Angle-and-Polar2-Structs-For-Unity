e626a628b532b8712d05a5f74c5f31053e90c060
rocky-digital <jamesroquelara12@gmail.com>
2022-10-20 23:34:05 -0500

    Hud working, ready to go public!

--- Assets/Scripts/CompassExample/DebugHUD.cs
+++ Assets/Scripts/CompassExample/DebugHUD.cs
@@ -7,13 +7,15 @@ using UnityEngine.UIElements;
 public class DebugHUD : MonoBehaviour
 {
     // User-defined variables
-    [Range(1, 15)] public int labelUpdatesPerSecond = 15;
+    [Range(1, 144)] public int labelUpdatesPerSecond = 60;
 
     // Private variables
     public CompassPointer compass;
-    public Angle angle1;
+    public Angle lookAngle;
     private Label angleNamesLabel;
-    
+    private Label unsignedAnglesLabel;
+    private Label signedAnglesLabel;
+
 
     // Dependent variables
     private float secondsPerLabelUpdate;
@@ -21,7 +23,8 @@ public class DebugHUD : MonoBehaviour
 
     private void OnEnable()
     {
-        angle1 = new();
+        lookAngle = new();
+        lookAngle = GetComponent<CompassPointer>().lookAngle;
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
         angleNamesLabel = rootVisualElement.Q<Label>("AngleNamesLabel");
         angleNamesLabel.text =
@@ -38,15 +41,47 @@ public class DebugHUD : MonoBehaviour
             "BinaryDegrees\n" +
             "Quadrants\n" +
             "Sextants";
+        unsignedAnglesLabel = rootVisualElement.Q<Label>("UnsignedAnglesLabel");
+        signedAnglesLabel = rootVisualElement.Q<Label>("SignedAnglesLabel");
         secondsPerLabelUpdate = 1 / (float)labelUpdatesPerSecond;
         currentSecondsBeforeLabelUpdate = 0;
     }
 
     private void Update()
     {
+        secondsPerLabelUpdate = 1 / (float)labelUpdatesPerSecond;
+        lookAngle = GetComponent<CompassPointer>().lookAngle;
         currentSecondsBeforeLabelUpdate -= Time.deltaTime;
         if (currentSecondsBeforeLabelUpdate <= 0)
         {
+            unsignedAnglesLabel.text =
+                "Unsigned\n" +
+                lookAngle.Unsigned.Degrees.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Radians.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Arcminutes.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Arcseconds.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Grads.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Turns.ToString("F2") + "\n" +
+                lookAngle.Unsigned.HourAngles.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Winds.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Milliradians.ToString("F2") + "\n" +
+                lookAngle.Unsigned.BinaryDegrees.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Quadrants.ToString("F2") + "\n" +
+                lookAngle.Unsigned.Sextants.ToString("F2");
+            signedAnglesLabel.text =
+                "Signed\n" +
+                lookAngle.Signed.Degrees.ToString("F2") + "\n" +
+                lookAngle.Signed.Radians.ToString("F2") + "\n" +
+                lookAngle.Signed.Arcminutes.ToString("F2") + "\n" +
+                lookAngle.Signed.Arcseconds.ToString("F2") + "\n" +
+                lookAngle.Signed.Grads.ToString("F2") + "\n" +
+                lookAngle.Signed.Turns.ToString("F2") + "\n" +
+                lookAngle.Signed.HourAngles.ToString("F2") + "\n" +
+                lookAngle.Signed.Winds.ToString("F2") + "\n" +
+                lookAngle.Signed.Milliradians.ToString("F2") + "\n" +
+                lookAngle.Signed.BinaryDegrees.ToString("F2") + "\n" +
+                lookAngle.Signed.Quadrants.ToString("F2") + "\n" +
+                lookAngle.Signed.Sextants.ToString("F2");
             currentSecondsBeforeLabelUpdate %= secondsPerLabelUpdate;
             currentSecondsBeforeLabelUpdate += secondsPerLabelUpdate;
         }
